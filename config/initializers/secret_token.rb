728f410dddb82b2afcc482ffe8a7258e8fa8b37c
jasonconny <jasonconny@gmail.com>
2013-09-03 18:22:40 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chachitweets::Application.config.secret_token = '40bf3ec962866d9ced0b5a0e187300cbe74fe6d646b82055b8355826189c28c0b9c753782090c32d9fc363d875c2dc113595db5d752ecf42daf36f9a8511f16a'
