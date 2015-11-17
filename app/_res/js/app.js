09bbf55b99fb88b6be1469bbe629c9c00054b83a
Jason Conny <jason.conny@leoburnett.com>
2015-11-16 21:44:35 -0600

    fixed scrollTo to hide browser chrome on mobile devices

--- app/_res/js/app.js
+++ app/_res/js/app.js
@@ -112,7 +112,9 @@ ChachiTweets.setFontSize = function() {
 };
 
 ChachiTweets.hideURLbar = function() {
-	setTimeout(scrollTo, 0, 0, 1);
+	setTimeout(function () {
+		window.scrollTo(0, 1);
+	}, 1000);
 };
 
 $(document).ready(function() {
