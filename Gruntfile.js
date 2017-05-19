a7c79c8e438e1092a4954ff303b841f91238dcf3
Jason Conny <jasonconny@gmail.com>
2017-05-19 17:39:08 -0500

    Updating grunt tasks for modular deployments

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,100 +5,130 @@ module.exports = function(grunt) {
 	var description;
 
 	switch (character) {
-        case 'Chachi':
-            description = 'Words of wisdom from Fonzie\'s cousin Chachi Arcola.';
-            break;
-        default:
-            description = 'blah';
-            break;
-    }
+		case 'Chachi':
+			description = 'Words of wisdom from Fonzie\'s cousin Chachi Arcola.';
+			break;
+		case 'Fonzie':
+			description = 'Come on Yolanda what\'s Fonzie Like?';
+			break;
+		default:
+			description = 'blah';
+			break;
+	}
 
 	grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        clean: ['./dist/*', '!./dist/.keep'],
-        copy: {
-            resources: {
-                expand: true,
-                cwd: './src/_res/',
-                src: ['**/*', '!**/sass/**'],
-                dest: './dist/_res/'
-            },
-            misc: {
-                expand: true,
-                cwd: './src/',
-                src: ['*.php', '*.ico', '*.png', 'robots.txt'],
-                dest: './dist/'
-            }
-        },
-        sass: {
-            dist: {
-                options: {
-                    "style" : "expanded",
-                    "sourceMap" : true,
-                    "unixNewlines" : true,
-                    "noCache" : true
-                },
-                files: {
-                    'app/_res/css/main.css': 'src/_res/sass/main.scss'
-                }
-            }
-        },
-        watch: {
-            css: {
-                files: '**/*.scss',
-                tasks: ['sass'],
-                options: {
-                    livereload: true
-                }
-            },
-            js: {
-                files: '**/*.js',
-                options: {
-                    livereload: true
-                }
-            },
-            html: {
-                files: '**/*.html',
-                options: {
-                    livereload: true
-                }
-            }
-        },
-        targethtml: {
-            dev: {
-                options: {
-                    curlyTags: {
-                        environment: 'dev.' + characterLower + 'tweets.com',
-                        gaid: 'UA-2569982-8',
-                        character: character,
-                        characterLower: characterLower,
-                        description: description
-                    }
-                },
-                files: {
-                    "./dist/index.html" : "./src/index.html"
-                }
-            },
-            prod: {
-                options: {
-                    curlyTags: {
-                        environment: 'www.chachitweets.com',
-                        gaid: 'UA-2569982-6'
-                    }
-                },
-                files: {
-                    "./dist/index.html" : "./src/index.html"
-                }
-            }
-        }
-    });
+		pkg: grunt.file.readJSON('package.json'),
+		clean: ['./dist/*', '!./dist/.keep'],
+		copy: {
+			js: {
+				expand: true,
+				cwd: './src/_res/',
+				src: ['js/*'],
+				dest: './dist/_res/'
+			},
+			resources: {
+				expand: true,
+				cwd: './src/_res/',
+				src: ['**/*', '!**/sass/**'],
+				dest: './dist/_res/'
+			},
+			misc: {
+				expand: true,
+				cwd: './src/',
+				src: ['*.php', '*.ico', '*.png', 'robots.txt'],
+				dest: './dist/'
+			}
+		},
+		sass: {
+			dist: {
+				options: {
+					'style' : 'expanded',
+					'sourceMap' : true,
+					'unixNewlines' : true,
+					'noCache' : true
+				},
+				files: {
+					'./dist/_res/css/chachi.css' : './src/_res/sass/chachi.scss',
+					'./dist/_res/css/fonzie.css' : './src/_res/sass/fonzie.scss'
+				}
+			}
+		},
+		watch: {
+			options: {
+				interval: 300
+			},
+			css: {
+				files: '**/*.scss',
+				tasks: ['sass'],
+				options: {
+					livereload: true
+				}
+			},
+			js: {
+				files: '**/*.js',
+				tasks: ['copy:js'],
+				options: {
+					livereload: true
+				}
+			},
+			html: {
+				files: '**/*.html',
+				tasks: ['targethtml:' + target],
+				options: {
+					livereload: true
+				}
+			}
+		},
+		targethtml: {
+			dev: {
+				options: {
+					curlyTags: {
+						character: character,
+						characterLower: characterLower,
+						description: description,
+						environment: characterLower + 'tweets.dev'
+					}
+				},
+				files: {
+					'./dist/index.html' : './src/index.html'
+				}
+			},
+			stage: {
+				options: {
+					curlyTags: {
+						character: character,
+						characterLower: characterLower,
+						description: description,
+						environment: 'stage.' + characterLower + 'tweets.com',
+						gaid: 'UA-2569982-8'
+					}
+				},
+				files: {
+					'./dist/index.html' : './src/index.html'
+				}
+			},
+			prod: {
+				options: {
+					curlyTags: {
+						character: character,
+						characterLower: characterLower,
+						description: description,
+						environment: 'www.' + characterLower + 'tweets.com',
+						gaid: 'UA-2569982-6'					}
+				},
+				files: {
+					'./dist/index.html' : './src/index.html'
+				}
+			}
+		}
+	});
 
 	grunt.loadNpmTasks('grunt-contrib-clean');
 	grunt.loadNpmTasks('grunt-contrib-copy');
 	grunt.loadNpmTasks('grunt-sass');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-targethtml');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-targethtml');
 
-    grunt.registerTask('default', ['watch']);
-    grunt.registerTask('build', ['sass', 'clean', 'targethtml:' + target, 'copy:misc', 'copy:resources']);
+	grunt.registerTask('default', ['watch']);
+	grunt.registerTask('build', ['clean', 'sass', 'targethtml:' + target, 'copy:misc', 'copy:resources']);
 };
